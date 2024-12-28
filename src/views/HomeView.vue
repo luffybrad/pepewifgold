28b51ebf442752e35ed08c16761dcd417fc89f59
luffybrad <bradleykahoron399@gmail.com>
2024-12-28 22:03:07 +0300

    First commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import logo from '@/assets/images/pepewifgold.jpg'
 import CoinButton from '@/components/CoinButton.vue';
 
 
 </script>
 <template>
   <v-container
-  class="fill-height"
   >
   <CoinButton/>
   </v-container>
