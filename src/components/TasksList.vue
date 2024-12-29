a8c06344cee9ffa2bd14cfd745ec921428a6bb90
luffybrad <bradleykahoron399@gmail.com>
2024-12-30 01:09:26 +0300

    Mods

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -3,7 +3,19 @@
 
 <template>
   <v-container>
+    <v-list>
+      <v-list-item
+      prepend-icon="mdi-youtube"
+      >
+      <v-list-item-title>
+        Subscribe
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        2500
+      </v-list-item-subtitle>
 
+      </v-list-item>
+    </v-list>
   </v-container>
 </template>
 <style scoped>
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import TasksList from '@/components/TasksList.vue';
+
 </script>
 
 <template>
   <v-layout>
-
+    <TasksList/>
   </v-layout>
 </template>
 <style scoped>
