6a53e0b90ddee9ce09e82277071f29881576b5ee
luffybrad <bradleykahoron399@gmail.com>
2024-12-30 10:47:05 +0300

    Mods

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -3,9 +3,30 @@
 
 <template>
   <v-container>
-    <v-list>
+
+    <span
+      class="d-flex justify-center text-green text-h4"
+
+      >
+      Invite Friends
+    </span>
+    <v-divider
+    class="my-5"
+    thickness="1"
+    color="success"
+    opacity="10"
+    >
+
+    </v-divider>
+
+    <v-list
+    >
       <v-list-item
       prepend-icon="mdi-youtube"
+      class="text-red red my-5"
+      variant="tonal"
+      rounded="xl"
+      append-icon="mdi-chevron-right"
       >
       <v-list-item-title>
         Subscribe
@@ -15,7 +36,73 @@
       </v-list-item-subtitle>
 
       </v-list-item>
+
+      <v-list-item
+      prepend-icon="mdi-twitter"
+      class="text-black black my-5"
+      variant="tonal"
+      rounded="xl"
+      append-icon="mdi-chevron-right"
+      >
+      <v-list-item-title>
+        Follow Us
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        2500
+      </v-list-item-subtitle>
+
+      </v-list-item>
+
+      <v-list-item
+      prepend-icon="mdi-facebook"
+      class="text-blue-darken-4 blue-darken-4 my-5"
+      variant="tonal"
+      rounded="xl"
+      append-icon="mdi-chevron-right"
+      >
+      <v-list-item-title>
+        Follow Us
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        2500
+      </v-list-item-subtitle>
+
+      </v-list-item>
+
+      <v-list-item
+      prepend-icon="mdi-instagram"
+      class="text-pink-accent-3 pink-accent-3 my-5"
+      variant="tonal"
+      rounded="xl"
+      append-icon="mdi-chevron-right"
+      >
+      <v-list-item-title>
+        Follow Us
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        2500
+      </v-list-item-subtitle>
+
+      </v-list-item>
+
+      <v-list-item
+      prepend-icon="mdi-send"
+      class="text-light-blue light-blue my-5"
+      variant="tonal"
+      rounded="xl"
+      append-icon="mdi-chevron-right"
+      >
+      <v-list-item-title>
+        Follow Us
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        2500
+      </v-list-item-subtitle>
+
+      </v-list-item>
+
     </v-list>
+
   </v-container>
 </template>
 <style scoped>
